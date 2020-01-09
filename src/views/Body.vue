6ab5cd3bd697819c0204b93d557d0f43e638ddbd
matthew <homekeeper89@gmail.com>
2020-01-10 07:09:49 +0900

    [fix] item 컴포넌트 기본값 추가
    스켈레톤 타입처럼 개발

--- src/views/Body.vue
+++ src/views/Body.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="body-main">
-    
+    <Item></Item>
   </div>
 </template>
+<script lang="ts">
+import Vue from 'vue'
+import {Component} from 'vue-property-decorator';
+import Item from '@/components/Item.vue'
+
+@Component({
+  components:{
+    Item
+  }
+})
+export default class Body extends Vue{
+  
+}
+</script>
