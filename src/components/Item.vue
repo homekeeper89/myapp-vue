6e5cf7bdcaada3b9ee4cc17943124edf6957875b
matthew <homekeeper89@gmail.com>
2020-01-10 07:09:25 +0900

    [new] re-usable components 추가
    todo 항목용으로

---
+++ src/components/Item.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="item-main">
+    <div class="input-group">
+      <span class="input-group-addon">
+        <input type="checkbox" aria-label="...">
+      </span>
+      <input type="text" class="form-control" aria-label="Amount (to the nearest dollar)" />
+      <span class="input-group-btn">
+        <button class="btn btn-default" type="button">X</button>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+
+@Component
+export default class Item extends Vue {}
+</script>
