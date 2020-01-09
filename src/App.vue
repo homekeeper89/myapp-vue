d43c0eb96c113292496e98a3305f0dac2d70d80e
matthew <homekeeper89@gmail.com>
2020-01-10 01:35:14 +0900

    [fix] router 위한 코드 정리
    body만 router-view로 추가

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,25 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <Header></Header>
+    <sidebar></sidebar>
     <router-view/>
   </div>
 </template>
+<script lang="ts">
+import Vue from 'vue'
+import {Component} from 'vue-property-decorator';
+import sidebar from '@/views/SideBar.vue';
+import Header from '@/views/Header.vue';
 
+@Component({
+  components:{
+    sidebar, Header
+  }
+})
+export default class App extends Vue{
+  
+}
+</script>
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
@@ -15,18 +27,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,22 +2,16 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
 
+import Body from '../views/Body.vue'
+
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: Home,
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  },
+    name: 'Body',
+    component: Body,
+  }
 ];
 
 const router = new VueRouter({
