53c818f81276bce147018cdeee67fdb60ba13024
matthew <homekeeper89@gmail.com>
2020-01-10 01:34:28 +0900

    [new] view에 기본 layout 추가
    얘들은 components로 넘어가야할듯

---
+++ src/views/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="header-main">
+    <div class="input-group">
+      <span class="input-group-addon" id="basic-addon1">@</span>
+      <input
+        type="text"
+        class="form-control"
+        placeholder="Username"
+        aria-describedby="basic-addon1"
+      />
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+
+@Component
+export default class Header extends Vue {}
+</script>
---
+++ src/views/SideBar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="sidebar-main">
+    i am sidebar
+  </div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+import {Component} from 'vue-property-decorator';
+
+@Component
+export default class SideBar extends Vue{
+  
+}
+</script>
