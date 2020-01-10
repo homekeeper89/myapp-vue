e3a771b0b957ba88c2c57cc3866dc855c20d9e52
matthew <homekeeper89@gmail.com>
2020-01-11 01:25:11 +0900

    [new] router-lint to 추가
    /:status를 통해서 동적 파람 설정

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,25 @@
 <template>
   <div class="header-main">
-    <div class="page-header">
+    <div class="page-header header-main__title">
       <h1>
         {{headerTitle}}
         <small>{{headerSubTitle}}</small>
       </h1>
     </div>
-    <div class="input-group">
+    <div class="header-main__nav">
+      <ul class="nav nav-tabs">
+        <li role="presentation" class="active">
+          <router-link to="/">All</router-link>
+        </li>
+        <li role="presentation">
+          <router-link to="/active">Active</router-link>
+        </li>
+        <li role="presentation">
+          <router-link to="/clear">clear</router-link>
+        </li>
+      </ul>
+    </div>
+    <div class="input-group header-main__inputGroup">
       <span class="input-group-addon" id="basic-addon1">@</span>
       <input
         type="text"
--- src/router/index.ts
+++ src/router/index.ts
@@ -8,7 +8,7 @@ Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
+    path: '/:status?',
     name: 'Body',
     component: Body,
   }
