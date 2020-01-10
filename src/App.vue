ebc8e7cc1e0df0649df018c5fa08d4545470312c
matthew <homekeeper89@gmail.com>
2020-01-11 00:49:03 +0900

    [fix] sidebar는 이번 개발에는 포함 안시키기로

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,17 @@
 <template>
   <div id="app">
     <Header></Header>
-    <sidebar></sidebar>
     <router-view/>
   </div>
 </template>
 <script lang="ts">
 import Vue from 'vue'
 import {Component} from 'vue-property-decorator';
-import sidebar from '@/components/SideBar.vue';
 import Header from '@/components/Header.vue';
 
 @Component({
   components:{
-    sidebar, Header
+    Header
   }
 })
 export default class App extends Vue{
