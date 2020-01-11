df174b970204b5c6f380f13327a80bc484d3c7ea
matthew <homekeeper89@gmail.com>
2020-01-11 19:26:25 +0900

    [new] vuex 적용함
    getter에서 param을 사용하려면 (state)=>(param)=>{doSomething)
    list를 watch로 사용하려면 처음에 init으로 [] 로 해줘야한다...

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,33 +1,52 @@
 import Vue from 'vue';
 import Vuex, { StoreOptions } from 'vuex';
-import {State, Item} from '@/store/interface/item.inteface';
+import { State, Item } from '@/store/interface/item.inteface';
 
 Vue.use(Vuex)
 
-const store :StoreOptions<State> = {
-  state:{ 
-    itemList:[]
+const store: StoreOptions<State> = {
+  state: {
+    itemList: [
+      {
+        id: 1,
+        title: "Make My Todo App :",
+        name: "this name",
+        status: 'active',
+      },
+      {
+        id: 2,
+        title: 'Make My Service',
+        name: 'this is my name',
+        status: 'active',
+      },
+      {
+        id: 3,
+        title: 'Do something good for me',
+        name: 'this is my name',
+        status: 'clear',
+      }
+    ]
   },
-  mutations:{
-    addItemToList(state:State, item:Item){
+  mutations: {
+    addItemToList(state: State, item: Item) {
       state.itemList.push(item);
     },
-    changeItemStatus(state, {id, status}:{id:number, status:"active"|"clear"|"waiting"}){
+    changeItemStatus(state, { id, status }: { id: number, status: "active" | "clear" | "waiting" }) {
       state.itemList[id].status = status;
     },
-    removeItem(state, id:number){
-      state.itemList.splice(id,1);
+    removeItem(state, id: number) {
+      state.itemList.splice(id, 1);
     }
   },
-  actions:{
+  actions: {
 
   },
-  getters:{
-    getAllItem(state){
+  getters: {
+    allItems(state) {
       return state.itemList;
     },
-    getItemOnStatus(state, status:"active"|"clear"|"waiting"){
-      return state.itemList.filter((item)=>item.status === status);
+    itemsOnStatus:(state)=>(status: "active" | "clear" | "waiting")=>{
+      return state.itemList.filter((item)=>item.status == status);
     }
   }
 }
--- src/views/Body.vue
+++ src/views/Body.vue
@@ -7,42 +7,36 @@
 import Vue from 'vue'
 import {Component, Watch} from 'vue-property-decorator';
 import Item from '@/components/Item.vue'
+import {mapGetters} from 'vuex';
 
 @Component({
   components:{
     Item
+  },
+  computed:{
+    ...mapGetters([
+      'allItems',
+      'itemsOnStatus'
+    ]
+    )
   }
 })
 export default class Body extends Vue{
-  items:any[]=[{
-      id:1,
-      title:"Make My Todo App :",
-      name:"this name",
-      status:'active',
-    },
-    {
-      id:2,
-      title:'Make My Service',
-      name:'this is my name',
-      status:'active',
-    },
-    {
-      id:3,
-      title:'Do something good for me',
-      name:'this is my name',
-      status:'clear',
-    }
-  ]
 
-  watchedItemList:any[] = this.items;
+  watchedItemList:any[]=[]
+
+  created(){
+    this.watchedItemList = this.allItems
+  }
 
   @Watch("$route.params.status")
   renderItemListByStatus(status:string|undefined){
     if(!status){
-      this.watchedItemList = this.items;
-      return;
+      this.watchedItemList = this.allItems;
+    }else{
+      this.watchedItemList = this.itemsOnStatus(status);
+      console.log(this.watchedItemList)
     }
-    this.watchedItemList = this.items.filter((item:any)=>item.status == status)
   }
 
 }
