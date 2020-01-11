d2cbce97612bf08c895bc49fc60e992684002c56
matthew <homekeeper89@gmail.com>
2020-01-11 19:40:13 +0900

    [new] addItem 은 Item type을 받는다

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -20,19 +20,22 @@
       </ul>
     </div>
     <div class="input-group header-main__inputGroup">
-      <span class="input-group-addon" id="basic-addon1">@</span>
+      <span class="input-group-addon" id="basic-addon1">입력</span>
       <input
         type="text"
         class="form-control"
         placeholder="username"
         aria-describedby="basic-addon1"
         data-cy="header-input-name"
+        v-model="username"
       />
       <input
         type="text"
         class="form-control"
         placeholder="title"
         aria-describedby="basic-addon1"
+        v-model="title"
+        v-on:keyup.enter="addItem"
         data-cy="header-input-title"
       />
     </div>
@@ -41,10 +44,23 @@
 <script lang="ts">
 import Vue from "vue";
 import { Component } from "vue-property-decorator";
+import { Item } from '@/store/interface/item.inteface';
 
 @Component
 export default class Header extends Vue {
   headerTitle: string = "My Todo Project";
   headerSubTitle: string = "Type what You do";
+  username:string = ""
+  title:string =""
+
+  addItem(){
+    let item = {
+      name:this.username,
+      title:this.title,
+      status:"waiting",
+      id:this.$store.state.itemList.length + 1
+    }
+    this.$store.commit('addItemToList', item)
+  }
 }
 </script>
